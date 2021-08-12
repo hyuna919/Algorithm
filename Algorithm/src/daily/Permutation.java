1dff68f1250b2bd42560c13101188f39d02b04e4
hyuna919 <hyuna919@naver.com>
2021-08-12 11:22:07 +0900

    daily

--- Algorithm/src/daily/Permutation.java
+++ Algorithm/src/daily/Permutation.java
@@ -1,24 +1,30 @@
 package daily;
 
+import java.util.Arrays;
+
 public class Permutation {
 	static int n=3, r=3;
-	static int [] inputs = {1,2,3}, numbers = new int[r];
+	static int [] inputs , nums;
 	public static void main(String[] args) {
+		inputs = new int[] {1,2,3};
+		nums = new int[r];
 		
+		permutation(0,0);
 		
 		
 	}
 	
-	public void permutation(int cnt, int flag) {
-		if(cnt ==0) {
+	public static void permutation(int cnt, int flag) {
+		if(cnt==r) {
+			System.out.println(Arrays.toString(nums));
 			return;
 		}
 		
-		for(int i = 1 ;i<=n;i++) {
-			if((flag & 1<<i)==0) {
-				numbers[cnt] = i;
-				permutation(cnt+1, (flag|1<<i));
-			}
+		for (int i = 0; i < n; i++) {
+			if((flag&1<<i)!=0) continue;
+			
+			nums[cnt] = inputs[i];
+			permutation(cnt+1, flag|1<<i);
 		}
 	}
 }
