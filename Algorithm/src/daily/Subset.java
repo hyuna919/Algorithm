ca51bbc042678fe0369b8c624ca1d68b1c505924
hyuna919 <hyuna919@naver.com>
2021-08-16 23:57:01 +0900

    commit

--- Algorithm/src/daily/Subset.java
+++ Algorithm/src/daily/Subset.java
@@ -1,25 +1,28 @@
 package daily;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Subset {
 	static int n;
-	static int[] subset;
-	static String[] inputs = "abcd".split("");
+	static String[] subset;
+	static String[] input;
 	
 	public static void main(String[] args) {
 		n = 5;
-		subset = new int[n];
+		input = "abcde".split("");
+		subset = new String[]{"-","-","-","-","-"};
 		
-		for (int i = 0, end=1<<n ; i<end ; i++) {
+		for (int i = 0, end=1<<n; i < end; i++) {
 			for (int j = 0; j < n; j++) {
 				if((i&1<<j)!=0) {
-					subset[j] = 1;
+					System.out.print(input[j]);		
 				}
 			}
-			print(subset);
-			Arrays.fill(subset, 0);
+			System.out.println();
+			Arrays.fill(subset, "-");
 		}
+		
 		
 	}
 
@@ -28,7 +31,7 @@ public class Subset {
 		System.out.println("[");
 		for (int s : subset) {
 			if(s!=0) {
-				System.out.println(inputs[k]+" ");
+				System.out.println(input[k]+" ");
 			}
 			k++;
 		}
