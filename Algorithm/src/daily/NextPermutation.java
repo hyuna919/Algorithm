3f1995d380003c88485b1c48dff6aac1afe72c0e
hyuna919 <hyuna919@naver.com>
2021-08-20 09:30:23 +0900

    0820 daily

--- Algorithm/src/daily/NextPermutation.java
+++ Algorithm/src/daily/NextPermutation.java
@@ -7,33 +7,30 @@ public class NextPermutation {
 	
 	
 	 public static void main(String[] args) {
-		 input = new int[]{1,2,3,4};
-		 
-		 do {
-			 System.out.println(Arrays.toString(input));
-		 }while(np());
-
-		 
+		input = new int[] {1,2,3,4};
+		
+		do {
+			System.out.println(Arrays.toString(input));
+		}while(np());
+		
 	 }
 
 
 	private static boolean np() {
-		int i = N-1;
-		while(i>0 && input[i-1]>=input[i])i--;
+		int i = N;
+		while(i>0 && input[i-1]>input[i]) i--;
 		
 		if(i==0) return false;
 		
-		int j = N-1;
-		while(input[i-1] >= input[j]) j--;
+		int j = N;
+		while(input[i-1] > input[j]) j--;
+		
 		swap(i-1,j);
 		
-		int k = N-1;
-		while(i<k) {
-			swap(i++,k--);
-		}
+		int k = N;
+		while(i<k) swap(i++, k--);
 		
 		return true;
-		
 	}
 
 
