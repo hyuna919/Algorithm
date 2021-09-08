f2f93267a6e79ea5a5af83b06e5570495e41d446
hyuna919 <hyuna919@naver.com>
2021-09-08 23:48:36 +0900

    .

--- Algorithm/src/day_0907/Main_BOJ_16236.java
+++ Algorithm/src/day_0907/Main_BOJ_16236.java
@@ -4,13 +4,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+class Shark{
+	int x, y , size=2, eat=0, dist;
+
+	public Shark(int x, int y, int dist) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.dist = dist;
+	}
+}
+
+class Fish{
+	int x, y, size;
+
+	public Fish(int x, int y, int size) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.size = size;
+	}
+}
+
 public class Main_BOJ_16236 {
-	static int N, sharkSize=2, sharkEat, cntFish, x, y, cnt;
+	static int N, cnt;
 	static int[][] map;
-	static ArrayList<int[]> fishes = new ArrayList<>();
-	static ArrayList<int[]> canList = new ArrayList<>();
+	static boolean[][] visited;
+	static Shark shark;
+	static ArrayList<Fish> bigfishes = new ArrayList<>();
+	static ArrayList<Fish> smallfishes = new ArrayList<>();
+	
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +47,7 @@ public class Main_BOJ_16236 {
 		
 		N = Integer.parseInt(in.readLine());
 		map = new int[N][N];
+		visited = new boolean[N][N];
 
 		// 공간 입력 및 물고기 위치, 상어 위치 확인
 		int tmp;
@@ -29,57 +59,100 @@ public class Main_BOJ_16236 {
 				if(tmp==0) {	// 빈공간
 					continue;
 				}else if(tmp==9) {	// 상어
-					x=i;
-					y=j;
+					shark = new Shark(i,j,0);
+					map[i][j] = 0;
 				}else if(tmp<2) {	// 먹을 수 있는 물고기
-					cntFish++;
-					canList.add(new int[] {i,j});
+					smallfishes.add(new Fish(i,j,tmp));
 				}else {				// 먹을 수 없는 물고기
-					cntFish++;
-					fishes.add(new int[]{i,j});
-					
+					bigfishes.add(new Fish(i,j,tmp));
 				}
 			}
 		}
+
+		if(smallfishes.isEmpty()) {
+			System.out.println(0);
+		}
 		
-		// 먹을 수 있는 물고기 목록 돌면서 제일 가까운 것 찾기 
-		if(cntFish>0) {
-			int dist;
-			int[] fish;
-			int min = Integer.MAX_VALUE;
-			int min_idx=-1;
-			for (int i = 0; i < cntFish; i++) {
+		move_bfs();
+		
+		System.out.println();
+	}
+	
+	
+	static void move_bfs() {
+		Queue<Shark> q= new LinkedList<>();
+		visited[shark.x][shark.y] = true;
+		q.offer(shark);
+		
+		// 탐색
+		int nx, ny;
+		ArrayList<Fish> eatFish = new ArrayList<>();
+		while(!smallfishes.isEmpty()) {
+			Shark now = q.poll();
+			cnt++;
+			for (int i = 0; i < 4; i++) {
+				nx = now.x + dx[i];
+				ny = now.y + dy[i];
 				
-				move(i);
+				// 경계검사 + 물고기 크기비교 + 방문확인
+				if(nx<0 || nx>N-1) continue;
+				if(ny<0 || ny>N-1) continue;
+				if(map[nx][ny] > now.size) continue;
+				if(visited[nx][ny]) continue;
 				
-				if(sharkSize==sharkEat) levelup();
+				// 방문+먹을수있는지
+				q.offer(new Shark(nx, ny, now.dist+1));
+				visited[nx][ny] = true;
+				if(map[nx][ny]!=0) {
+					eatFish.add(new Fish(nx,ny,0));
+				}
+			}
+			
+			if(smallfishes.isEmpty()) {
+				return;
+			}
+
+			// 같은 거리에 있는 생선 중 더 위, 왼쪽에 있는 것
+			if(!eatFish.isEmpty()) {			
+				int min = 0;
+				for (int i = 1, end = eatFish.size(); i < end; i++) {
+					if(eatFish.get(min).x > eatFish.get(i).x) {
+						min = i;
+					}else if(eatFish.get(min).x == eatFish.get(i).x) {
+						if(eatFish.get(min).y > eatFish.get(i).y) {
+							min = i;
+						}
+					}
+				}
+				eat(eatFish.get(min));
+			}
+			
+		}
+		
+	}
+
+	static void eat(Fish fish) {
+		shark.x = fish.x;
+		shark.y = fish.y;
+		
+		shark.eat++;
+		visited = new boolean[N][N];
+		
+		for (int i = 0, end = smallfishes.size(); i < end; i++) {
+			if(smallfishes.get(i).x==fish.x && smallfishes.get(i).y==fish.y) {
+				smallfishes.remove(i);
 			}
 		}
 		
-		
-		System.out.println(cnt);
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-
-	private static void move(int i) {
-		int r = x, c = y;
-		
-		
-	}
-
-	private static void levelup() {
-		sharkEat = 0;
-		sharkSize++;
-		for (int[] is : fishes) {
-			if(map[is[0]][is[1]] < sharkSize) canList.add(is);
+		if(shark.eat == shark.size) {
+			shark.size++;
+			shark.eat = 0;
+			for (int i = bigfishes.size()-1; i >= 0; i--) {
+				if(bigfishes.get(i).size < shark.size) {
+					smallfishes.add(bigfishes.get(i));
+					bigfishes.remove(i);
+				}
+			}
 		}
 	}
-
 }
