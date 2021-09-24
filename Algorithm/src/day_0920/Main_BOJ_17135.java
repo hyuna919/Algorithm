5be321e22677accf7e9b3e2e4a19e895412f5a20
hyuna919 <hyuna919@naver.com>
2021-09-24 10:53:11 +0900

    자바 59위

--- Algorithm/src/day_0920/Main_BOJ_17135.java
+++ Algorithm/src/day_0920/Main_BOJ_17135.java
@@ -9,10 +9,21 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_BOJ_17135 {
-	static int N, M, D, max=0, kill;
+	static class Position{
+		int x,y,d;
+
+		public Position(int x, int y, int d) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.d = d;
+		}
+	}
+	static int N, M, D, max=0, kill, enemyOri, enemy;
 	static int[] archer = new int[3];
 	static int[][] mapOri, map;
-	static ArrayList<Integer> xKill, yKill;
+//	static ArrayList<Integer> killX, killY;
+	static ArrayList<int[]> killXY;
 	
 	// 좌상우
 	static int[] dx = {0,-1,0};
@@ -32,6 +43,7 @@ public class Main_BOJ_17135 {
 			token = new StringTokenizer(in.readLine());
 			for (int j = 0; j < M; j++) {
 				mapOri[i][j] = token.nextToken().equals("0")?0:1;
+				if(mapOri[i][j]==1)enemyOri++;
 			}
 		}
 		
@@ -39,8 +51,8 @@ public class Main_BOJ_17135 {
 		comb(M,3);
 		
 		System.out.println(max);
-		
 	}
+	
 	private static void comb(int n, int r) {
 		if(r==0) {
 			kill = 0;
@@ -62,73 +74,68 @@ public class Main_BOJ_17135 {
 		for (int i = 0; i < N; i++) {
 			map[i] = mapOri[i].clone();
 		}
-		
+		enemy = enemyOri;
 	}
 	private static void defense() {
 		for (int i = N; i > 0; i--) {
-			xKill = new ArrayList<Integer>();
-			yKill = new ArrayList<Integer>();
+//			killX = new ArrayList<Integer>();
+//			killY = new ArrayList<Integer>();
+			killXY = new ArrayList<int[]>();
+			
 			for (int j = 0; j < 3; j++) {
-				dfs(i,archer[j],D);
-				remove();
+				bfs(i,archer[j],D);
 			}
+			remove(i-1);
+			if(enemy==0) return;
 		}
 	}
 	
-	private static void remove() {
-		for (int i = xKill.size()-1; i > 0; i--) {
-			map[xKill.get(i)][yKill.get(i)] = 0;
+	private static void remove(int x) {
+//		for (int i = killX.size()-1; i > -1; i--) {
+//			map[killX.get(i)][killY.get(i)] = 0;
+//			enemy--;
+//		}		
+		for (int i = killXY.size()-1; i > -1; i--) {
+			map[killXY.get(i)[0]][killXY.get(i)[1]] = 0;
+			enemy--;
 		}		
-	}
-	
-	private static boolean dfs(int x, int y, int d) {
-		if(d==0) return false;
-		int nx, ny;
-		for (int i = 0; i < 3; i++) {
-			nx = x+dx[i];
-			ny = y+dy[i];
-			
-			// 경계값 검사
-			if(nx<0||nx>=N||ny<0||ny>=M) continue;
-			// 궁병과 같은 행은 검사하지 않음
-			if(nx==x && d==D) continue;
-			// 적이 있으면 죽이고 true반환(dfs완전 종료 위한 플래그)
-			if(map[nx][ny]==1) {
-				xKill.add(nx);
-				yKill.add(ny);
-				map[nx][ny] = 2;
-				kill++;
-				return true;
-			}else if(map[nx][ny]==2) {
-				return true;
+		for (int i = 0; i < M; i++) {
+			if(map[x][i]==1) {
+				map[x][i]=0;
+				enemy--;
 			}
 		}
-		return false;
 	}
 	
-	private static void bfs(int cur) {
-		Queue<Integer> queue = new LinkedList<Integer>();   //탐색할 노드를 담을 queue
+	private static void bfs(int x, int y, int d) {
+		Queue<Position> q = new LinkedList<Position>();
+		q.offer(new Position(x,y,d));
 		
-//		1. 첫 방문한 node를 queue에 담기 
-		queue.offer(cur);
-//		2. queue에 담은 node를 방문 처리 
-		visited[cur] = true;
-		
-		
-//		3. 탐색 시작 
-		//queue에 방문할 node를 담기 때문에 queue가 empty라는것는 모든 노드를 다 방문 했다는 의미,  empty가 아니면 계속 방문을 해야 한다. 
-		while(!queue.isEmpty()) {    
-			cur = queue.poll();			//현재 방문할 노드를 queue에서 꺼내 온다 
-			System.out.printf("%c ", cur+65);
+		Position now;
+		int nx, ny;
+		while(!q.isEmpty()) {    
+			now = q.poll();
+			if(now.d==0) continue;
 			
-//			인접한 node를 방문
-			for (int adj = 0; adj < n; adj++) {
-//				     인접 되어 있고                            인접 노드가  방문하지 않았다면 
-				if(map[cur][adj]&& !visited[adj]) {
-//					방문하기 위해 queue에 넣기 
-					queue.offer(adj);
-					visited[adj] = true;		//미리 방문 했다고 표시하기 
-				}
+			for (int i = 0; i < 3; i++) {
+				nx = now.x + dx[i];
+				ny = now.y + dy[i];
+				
+				// 경계값 검사
+				if(nx<0||nx>=N||ny<0||ny>=M) continue;
+				// 궁병과 같은 행은 검사하지 않음
+				if(nx==now.x && now.d==D) continue;
+				// 적이 있으면 죽이고 다음 궁병으로
+				if(map[nx][ny]==1) {
+//					killX.add(nx);
+//					killY.add(ny);
+					killXY.add(new int[] {nx,ny});
+					map[nx][ny] = 2;
+					kill++;
+					return;
+				}else if(map[nx][ny]==2) {
+					return;
+				}else q.offer(new Position(nx,ny,now.d-1));
 			}
 		}
 	}
