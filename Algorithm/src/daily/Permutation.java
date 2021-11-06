c7efe7759c8f08f625305e5c4e94c0af7e7a8b82
hyuna919 <hyuna919@naver.com>
2021-11-06 21:34:05 +0900

    daily

--- Algorithm/src/daily/Permutation.java
+++ Algorithm/src/daily/Permutation.java
@@ -3,11 +3,11 @@ package daily;
 import java.util.Arrays;
 
 public class Permutation {
-	static int n=3, r=2, cnt;
+	static int n=8, r=8, cnt;
 	static int [] inputs , nums;
 	public static void main(String[] args) {
 		nums = new int[r];
-		inputs = new int[] {1,2,3};
+		inputs = new int[] {1,2,3,4,5,6,7,8};
 		
 		perm(0,0);
 		System.out.println("케 "+cnt);
@@ -15,19 +15,34 @@ public class Permutation {
 	}
 	
 	public static void perm(int now, int flag){
-		if(now==r) {
+		if(now==8) {
 			cnt++;
-			System.out.println(Arrays.toString(nums));
 			return;
 		}
 		
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i < 9; i++) {
 			if((flag&1<<i)==0) {
+				if(now>0) System.out.println(nums[now-1]);
 				nums[now] = i;
 				perm(now+1, flag|1<<i);
 			}
 		}
 	}
+//	
+//	public static void perm(int now, int flag){
+//		if(now==r) {
+//			cnt++;
+//			System.out.println(Arrays.toString(nums));
+//			return;
+//		}
+//		
+//		for (int i = 0; i < n; i++) {
+//			if((flag&1<<i)==0) {
+//				nums[now] = i;
+//				perm(now+1, flag|1<<i);
+//			}
+//		}
+//	}
 	
 
 }
---
+++ Algorithm/src/daily/Permutation2.java
@@ -0,0 +1,19 @@
+package daily;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Permutation2 {
+	static int N, K, V;	// 지역수, 조합선택, 중계기
+	public static void main(String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token;
+		
+		
+	}
+
+	
+
+}
