7489dee6c246fff6cddcabe76134e08ecac34711
jay <49274191+hyuna919@users.noreply.github.com>
2022-01-22 23:22:57 +0900

    Update 표편집.java

--- src/programmers/표편집.java
+++ src/programmers/표편집.java
@@ -1,7 +1,7 @@
 package programmers;
 
 //https://programmers.co.kr/learn/courses/30/lessons/81303
-
+// 표 목록을 boolean[]으로 할 지, ArrayList<Integer>로 할 지 고민했는데 많은 행이 삭제된 상황에서 여러칸을 이동하는 문제 때문에 ArrayList<Integer>를 선택.
 
 public class 표편집 {
 
