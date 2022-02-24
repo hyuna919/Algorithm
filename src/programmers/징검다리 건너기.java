1b3fcf509d162021b0b9955475a422826b8de69d
jay <49274191+hyuna919@users.noreply.github.com>
2022-02-24 17:51:17 +0900

    Update 징검다리 건너기.java

--- src/programmers/징검다리 건너기.java
+++ src/programmers/징검다리 건너기.java
@@ -1,46 +1,63 @@
 ## 그리디하게 짰다
 ## 왜이렇게 어렵지...ㅎ
 ## 이진으로 풀어볼까?
-
+## 알고리즘 처음 풀때로 돌아간 것 같다. 설계에 구멍이 너무 
+    
 import java.util.*;
 
 class Solution {
     public int solution(int[] stones, int k) {
-        int cnt = 0;
-        int dist = 0;
+        int cnt, dist, res= Integer.MAX_VALUE;
         int n = stones.length;
-        int min = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE, max = 0;
         boolean flag = false;
-        // 최저값 빼기
+        
+        // 최저, 최고
         for(int now : stones){
-            min = min>now?now:min;
+            if(now<min) min = now;
+            if(now > max) max = now;
         }
-        for(int i=0;i<n;i++){
-            stones[i] -= min;
-        }
-        cnt = min;
         
-        // System.out.println(stones[0]);
+        // 중간값+gap
+        int mid = (min+max)/2;
+        int gap = mid;
         
-        while(true){
-            flag = false;
+        while(min <= max){
+            // gap 빼기
+            for(int i=0;i<n;i++){
+                stones[i] -= gap;
+            }            
+            
             dist = 0;
-            for(int i=0; i<n ; i++){
-                if(dist==k) {
-                    flag = true;
+            cnt = mid;
+            flag = true;
+            
+            
+            // 건널 수 있는지 아닌지 확인
+            for(int i=0;i<n;i++){
+                if(stones[i] <= 0) dist++;
+                else dist = 0;
+                
+                // 건널 수 없음
+                if(dist >= k) {
+                    flag = false;
                     break;
                 }
-                if(stones[i] == 0) {
-                    dist++;
-                }else{
-                    dist = 0;
-                    stones[i]-=1;
-                }
             }
-            if(flag) break;
-            cnt++;
+            
+            if(flag){   // 끝까지 다 건넌다면
+                min = mid + 1;
+                gap = (min+max)/2 - mid;
+                mid = (min+max)/2;
+            }else{      // 다 건너지 못하면
+                res = (res>cnt)?cnt:res;
+                max = mid - 1;
+                gap = (min+max)/2 - mid;
+                mid = (min+max)/2;
+            }
+            
         }
         
-        return cnt;
+        return res;
     }
 }
