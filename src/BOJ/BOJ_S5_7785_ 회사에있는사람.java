a7a0d536ba81a2eea549c3b77e036b68ae40aa15
jay <49274191+hyuna919@users.noreply.github.com>
2022-04-11 22:00:14 +0900

    Update BOJ_S5_7785_ 회사에있는사람.java

--- src/BOJ/BOJ_S5_7785_ 회사에있는사람.java
+++ src/BOJ/BOJ_S5_7785_ 회사에있는사람.java
@@ -1,22 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
+
 
 /*
 * map을 거의 안쓰다보니 연습하려고 map 푸는 중 2
 * 1- Hashset을 정렬하는 함수를 모르겠어서 검색했다.
 *    List로 생성해버리면 된다.
+* 2- treeset은 정말 처음 써보는듯하다.
+*    hashset보다 추가/삭제 시간 걸리지만 검색/정렬 유리
+*    이문제에서는 hashset이랑 큰 차이는 안느껴진다
 */
 
 public class Main {
 	static int N;
-	static Set<String> set = new HashSet<String>();
+	static TreeSet<String> set = new TreeSet<String>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer token;
@@ -33,14 +33,10 @@ public class Main {
 		}
 		
 		
-		int size = set.size();
-		set.iterator();
-		List<String> list = new ArrayList(set);
-		Collections.sort(list);
 		StringBuilder builder =new StringBuilder();
-		for (int i = size-1; i > -1; i--) {
-			builder.append(list.get(i)+"\n");
-		}
+		java.util.Iterator<String> itr = set.descendingIterator();
+		while(itr.hasNext()) builder.append(itr.next()+"\n");
+
 		System.out.println(builder.toString());
 	}
 }
