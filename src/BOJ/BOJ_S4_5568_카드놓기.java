4ecb0f094d67c09f3043fc31175454b32c965395
jay <49274191+hyuna919@users.noreply.github.com>
2022-04-11 21:38:45 +0900

    Update BOJ_S4_5568_카드놓기.java

--- src/BOJ/BOJ_S4_5568_카드놓기.java
+++ src/BOJ/BOJ_S4_5568_카드놓기.java
@@ -1,55 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 /*
 * map을 거의 안쓰다보니 연습하려고 map 푸는 중
 * 1- 그냥 순열로 풀었다. map적용 방안조차 안떠오름
 * 2- 우선 문자열로 쓰는거 
+* 3- set으로 바꿨다. 백준 기록 보면 알겠지만 메모리, 시간 모두 엄청 단축됐다. 깨달음을 얻음
 */
 
 public class Main {
 	static int N, K, cnt;
 	static int[] list;
-	static boolean[] isChecked;
+	static Set<Integer> isChecked = new HashSet<Integer>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
 		K = Integer.parseInt(br.readLine());
-		cnt = 0;
 		
-		
-		int len=0;
-		switch (K) {
-		case 2:
-			len = 99_99;
-			break;
-		case 3:
-			len = 99_99_99;
-			break;
-		case 4:
-			len = 9999_9999;
-			break;
-		}
-		isChecked = new boolean[len];
 		list = new int[N];
 		for (int i = 0; i < N; i++) {
 			list[i] = Integer.parseInt(br.readLine());
 		}
 		
 		// 순열
-		perm(0, 0, "");
-		
-		
-		System.out.println(cnt);
+		perm(0, 0, 0);
+		System.out.println(isChecked.size());
 	}
+	
 	private static void perm(int flag, int now, int num) {
 		if(now==K) {
-			if(!isChecked[num]) {
-				isChecked[num] = true;
-				cnt++;
-			}
+			isChecked.add(num);
 			return;
 		}
 		
