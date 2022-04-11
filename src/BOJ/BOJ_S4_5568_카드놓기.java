90ef32d4a7fdeddb02420b31e6467cb0032581c2
jay <49274191+hyuna919@users.noreply.github.com>
2022-04-11 21:33:28 +0900

    Update BOJ_S4_5568_카드놓기.java

--- src/BOJ/BOJ_S4_5568_카드놓기.java
+++ src/BOJ/BOJ_S4_5568_카드놓기.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 /*
 * map을 거의 안쓰다보니 연습하려고 map 푸는 중
 * 1- 그냥 순열로 풀었다. map적용 방안조차 안떠오름
+* 2- 우선 문자열로 쓰는거 
 */
 
 public class Main {
@@ -43,11 +44,10 @@ public class Main {
 		
 		System.out.println(cnt);
 	}
-	private static void perm(int flag, int now, String num) {
+	private static void perm(int flag, int now, int num) {
 		if(now==K) {
-			int tmp = Integer.parseInt(num);
-			if(!isChecked[tmp]) {
-				isChecked[tmp] = true;
+			if(!isChecked[num]) {
+				isChecked[num] = true;
 				cnt++;
 			}
 			return;
@@ -55,7 +55,8 @@ public class Main {
 		
 		for (int i = 0; i < N; i++) {
 			if((flag&1<<i)!=0) continue;
-			perm(flag|1<<i, now+1, num+list[i]);
+			if(list[i]>9) perm(flag|1<<i, now+1, (num*100)+list[i]);
+			else perm(flag|1<<i, now+1, (num*10)+list[i]);
 		}
 		
 	}
