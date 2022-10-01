3c9befaafa13f1e12bb5775410632ab91f3b5b40
jay <49274191+hyuna919@users.noreply.github.com>
2022-10-01 09:20:20 +0900

    Update decimal2binary.java

--- Standard/decimal2binary.java
+++ Standard/decimal2binary.java
@@ -1,4 +1,6 @@
-// 10진법->2진법으로 바꿀 때 2로 나눠서 바궜는데 toString을 쓰면 된다
+// 10진법->2진법으로 바꿀 때 2로 나눠서 바궜는데 Integer 내장함수 쓰자
 
 int N = 10;
-int binary = N.toString(2);
+String binaryString = Integer.toBinaryString(i); // 2진수
+String octalString = Integer.toOctalString(i);   // 8진수
+String hexString = Integer.toHexString(i);       // 16진수
