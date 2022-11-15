8af8bccafeabc5f3b1a946e41f97ddd3cf39e071
jay <49274191+hyuna919@users.noreply.github.com>
2022-11-15 21:33:41 +0900

    Create binarySearch.java

---
+++ Standard/binarySearch.java
@@ -0,0 +1,16 @@
+private static int binarySearch(int input) {
+    int left = 0;
+    int right = list.size()-1;
+    int mid = (left + right)/2;
+
+    int value;
+    while(left <= right) {
+        mid = (left + right)/2;
+        value = list.get(mid);
+
+        if(value > input) right = mid - 1;
+        else left = (mid++) + 1;
+    }
+
+    return mid;
+}
