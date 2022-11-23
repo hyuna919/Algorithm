fad07ec628e4f8a932c00cb90a2995393483454c
jay <49274191+hyuna919@users.noreply.github.com>
2022-11-23 22:19:50 +0900

    Update L3_양과늑대.java

--- src/programmers/L3_양과늑대.java
+++ src/programmers/L3_양과늑대.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 문제점) 2문제 틀림 why?
  */
+
 class Solution {
     static class State{
         int sheep, wolf;
@@ -23,13 +24,13 @@ class Solution {
     public int solution(int[] info, int[][] edges) {
         this.info = info;
         N = info.length;
+        M = N-1;    // == edges.length
 
         // 전체 양의 수 얻기, 늑대가 1
         maxSheep = N;
         for(int i:info) maxSheep -= i;
 
         // 트리 관계 만들기
-        M = N-1;    // == edges.length
         parents = new int[N];
         children = new int[N][2];
         for (int i = 0; i < M; i++) {
@@ -49,53 +50,49 @@ class Solution {
         for (int i = 0; i < N; i++) {
             visited[i] = new ArrayList<>();
         }
-        dfs(0,1,0);
+        dfs(0,1,0, -1);
 
         return max;
     }
 
-    private void dfs(int node, int sheep, int wolf) {
+    private void dfs(int node, int sheep, int wolf, int before) {
+
         // 같은 상태로 왔던 곳인가
         ArrayList<State> records = visited[node];
         for(State r:records) {
             if(r.sheep==sheep && r.wolf==wolf) return;
         }
+        // 방문 기록
         State now = new State(sheep,wolf);
-        records.add(now);
+        visited[node].add(now);
+        max = Math.max(max, sheep);
 
         // 늑대가 같거나 더 많아지는 경우 -> stop
-        if(sheep<=wolf){
-            max = Math.max(max, sheep);
-            return;
-        }
+        if(sheep <= wolf) return;
 
         // 전체 양을 모두 획득
-        if(sheep == maxSheep) {
-            max = maxSheep;
-            return;
-        }
         if(max == maxSheep) return;
 
         // 자식으로 이동
         for (int child:children[node]) {
-            // 자식이 있다면
-            if (child != 0) {
-                if (info[child] == 0) {
+            // 자식이 있다면 && 바로 직전에 온 곳이 아니면
+            if (child != 0 && child != before) {
+                if (info[child] == 0) { // 양이 있다
                     info[child] = -1;
-                    dfs(child, sheep + 1, wolf+0);
+                    dfs(child, sheep + 1, wolf, node);
                     info[child] = 0;
-                } else if (info[child] == 1) {
+                } else if (info[child] == 1) {  // 늑대가 있다
                     info[child] = -1;
-                    dfs(child, sheep, wolf + 1);
+                    dfs(child, sheep, wolf + 1, node);
                     info[child] = 1;
-                } if (info[child] == -1) {
-                    dfs(child, sheep, wolf);
+                } else {    // 빈자리다
+                    dfs(child, sheep, wolf, node);
                 }
             }
         }
 
-        // 부모로 이동
-        if(node!=0) dfs(parents[node], sheep, wolf);
+        // 부모로 이동(현재위치는 이전 dfs에서 먹고왔음)
+        if(node!=0) dfs(parents[node], sheep, wolf, node);
     }
 
 
