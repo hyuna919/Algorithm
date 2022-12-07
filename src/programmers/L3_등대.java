87abb03193b6e9f866e5e21e6a2c3696ce8feeab
jay <49274191+hyuna919@users.noreply.github.com>
2022-12-07 22:08:06 +0900

    Update L3_등대.java

--- src/programmers/L3_등대.java
+++ src/programmers/L3_등대.java
@@ -1,73 +1,102 @@
 import java.util.*;
 
-
+// n개의 노드, n-1개의 경로 -> 루프x -> 부모노드는 무조건 1개
 class Solution {
-    static int[][] list;
-    static int[] connectedCnt;
-    static boolean[] stateList, isLeaf;
-    static ArrayList<Integer>[] connected;
-    static Queue<Integer> q = new LinkedList<>();
+    static int root = 1, max=1;    // 완전, 이진 트리가 아니라서 루트 정하기 어려움. 임의로 지정할 것.
+    static Map<Integer,ArrayList<Integer>> tree;
+    static int[] countRoute, parents;
+    static ArrayList<Integer> leafList;
+    static boolean[] stateList;
 
     public int solution(int n, int[][] lighthouse) {
-        list = lighthouse;
+        countRoute = new int[n + 1];  // 1~n번 등대
+        parents = new int[n + 1];     // 1~n번 등대
+        leafList = new ArrayList<>();
+        tree = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            tree.put(i, new ArrayList<>());
+        }
 
-        stateList = new boolean[n+1];
-        isLeaf = new boolean[n+1];
-        connectedCnt = new int[n+1];
-        connected = new ArrayList[n+1];
-        for (int i = 0, end=connected.length; i < end; i++) {
-            connected[i] = new ArrayList<>();
+        // 연결 등대 정리 + 경로 세기
+        for (int[] route : lighthouse) {
+            int a = route[0];
+            int b = route[1];
+
+            tree.get(a).add(b);
+            tree.get(b).add(a);
+
+            countRoute[a]++;
+            countRoute[b]++;
+        }
+
+        // 리프 목록 만들기 + 연결 가장 많은 노드를 루트로 잡기
+        for (int i = 1; i <= n; i++) {
+            if (countRoute[i] == 1) leafList.add(i);
+            if (countRoute[i] > max) {
+                root = i;
+                max = countRoute[i];
+            }
         }
 
 
-        // 트리만들기
-        for (int i = 0, end=list.length; i < end; i++) {
-            int a = list[i][0];
-            int b = list[i][1];
-            connectedCnt[a]++;
-            connectedCnt[b]++;
-            connected[a].add(b);
-            connected[b].add(a);
-        }
+        // 부모배열만들기 -> parents[route]는 0이 됨
+        if(n==2){
+            parents[2] = 1;
+        } else {
+            boolean[] visited = new boolean[n + 1];
+            Queue<Integer> q = new LinkedList<>();
+            for (int node:leafList) {
+                q.add(node);
+            }
 
-        // 리프로부터 필수인 등대 켜두기
-        for (int i = 0, end=connectedCnt.length; i < end; i++) {
-            // 연결된 등대가 1개 -> 리프노드 -> 연결된 등대 추가
-            if(connectedCnt[i]==1) {
-                isLeaf[i] = true;
-                if(!stateList[i]){
-                    int next = connected[i].get(0);
-                    stateList[next] = true;
-                    q.offer(next);
+            while(!q.isEmpty()) {
+                int now = q.poll();
+
+                if(now==root) continue;
+
+                // 방문처리
+                if(visited[now]) continue;
+                visited[now] = true;
+
+                ArrayList<Integer> list = tree.get(now);
+                for(int next:list) {
+                    if(!visited[next]) {
+                        parents[now] = next;
+                        q.add(next);
+                    } else if(next==root) parents[now] = next;
                 }
             }
         }
 
-        turnOn();
+
+        // 등 켜기(리프->부모방향)
+        Queue<Integer> q = new LinkedList<>();
+        stateList = new boolean[n+1];
+        for (int node:leafList) {
+            q.offer(node);
+
+            while(!q.isEmpty()){
+                int now = q.poll();
+                if(now==root) break;
+                // now(자식)이 꺼져있으면 부모를 켠다
+                int parent = parents[now];
+                if(!stateList[now]){
+                    stateList[parent] = true;
+                }
+
+                // now의 상태와 무관하게 무조건 조부모 노드가 q에 추가된다.
+                // 부모가 루트면 조부모가 없다
+                if(parent!=root) q.add(parents[parent]);
+            }
+        }
+
 
         // 켜져있는 등대 세기
         int answer = 0;
-        for (boolean state:stateList) if(state) answer++;
+        for(boolean s:stateList){
+            if(s) answer++;
+        }
         return answer;
     }
 
-    private void turnOn() {
-        // q에 있는 등대 키고+2다리 건너 등대 추가하기
-        while (!q.isEmpty()){
-            int now = q.poll();
-            if(stateList[now]){ // 현재 등대가 켜져있으면
-                // 다다음 등대
-                for(int next : connected[now]){
-                    for(int nextnext : connected[next]){
-                        if(nextnext==now) continue; // 다다음이 지금이면 continue;
-                        q.offer(nextnext);
-                    }
-                }
-            } else {    // 현재 등대가 꺼져있으면
-                // 
-            }
-
-
-        }
-    }
 }
