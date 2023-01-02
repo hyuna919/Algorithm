6de17aeb382368339449b58c6a39c2a049626444
jay <49274191+hyuna919@users.noreply.github.com>
2023-01-02 22:27:58 +0900

    Update BOJ_S3_2606_바이러스.java

--- src/BOJ/BOJ_S3_2606_바이러스.java
+++ src/BOJ/BOJ_S3_2606_바이러스.java
@@ -2,45 +2,40 @@ import java.io.*;
 import java.util.*;
 
 /*
-BOJ_S3_2606_바이러스
+BOJ_S2_11279_최대힙
+
+PriorityQueue PQ Comparator
+PriorityQueue PQ 간단하게
  */
 public class Main {
-    static int N, M;
-    static boolean[] isVirus;
-    static boolean[][] links;
+    static int N;
     public static void main(String[] args) throws IOException {
         // 0. 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        isVirus = new boolean[N+1];
-        links = new boolean[N+1][N+1];
-        for (int i = 0; i < M; i++) {
-            StringTokenizer token = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(token.nextToken());
-            int b = Integer.parseInt(token.nextToken());
-            links[a][b] = true;
-            links[b][a] = true;
-        }
 
-        // 1. 감염
-        isVirus[1] = true;
-        process(1);
+        StringBuilder builder = new StringBuilder();
 
-        // 결과
-        int cnt = -1;
-        for(boolean i:isVirus) if(i) cnt++;
-        System.out.println(cnt);
-    }
+//        PriorityQueue<Integer> heap = new PriorityQueue<Integer>(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o2-o1;
+//            }
+//        });
 
-    private static void process(int master) {
-        for (int i = 1; i <= N; i++) {
-            if(i==master) continue;
-            if(isVirus[i]) continue;    // 이미 감염된 곳은 패스
-            if(links[master][i]) {      // 연결관계
-                isVirus[i] = true;
-                process(i);
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if(x==0) {
+                if(heap.isEmpty()) builder.append(0).append("\n");
+                else builder.append(heap.poll()).append("\n");
             }
+            else heap.offer(x);
         }
+
+        System.out.println(builder);
     }
+
+
 }
